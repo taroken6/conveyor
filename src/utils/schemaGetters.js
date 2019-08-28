11d867d5ca2551abe66eb6b2e76f3df4a7fd9165
Thomas Worley <thomas.worley96@gmail.com>
2019-08-28 10:28:45 -0700

    Split field order to be overridable for each page

--- src/utils/schemaGetters.js
+++ src/utils/schemaGetters.js
@@ -43,7 +43,14 @@ export const getRequiredFields = (schema, modelName) => {
   return getShownFields({ schema, modelName, type: 'required' })
 }
 
-export const getCreateFields = (schema, modelName) => {
+export const getCreateFields = ({ schema, modelName }) => {
+  const createFieldOrder = R.prop('createFieldOrder', getModel(schema, modelName))
+  if (R.type(createFieldOrder) === 'Function') {
+    return createFieldOrder({ schema, modelName })
+  }
+  else if (R.type(createFieldOrder) === 'Array') {
+    return createFieldOrder
+  }
   return getShownFields({ schema, modelName, type: 'showCreate' })
 }
 
@@ -52,11 +59,23 @@ export const getHasIndex = (schema, modelName) => {
 }
 
 export const getDetailFields = ({ schema, modelName, node }) => {
-  return getShownFields({ schema, modelName, type: 'showDetail', node })
+  const detailFieldOrder = R.prop('detailFieldOrder', getModel(schema, modelName))
+  if (R.type(detailFieldOrder) === 'Function') {
+    return detailFieldOrder({ schema, modelName })
+  }
+  else if (R.type(detailFieldOrder) === 'Array') {
+    return detailFieldOrder
+  }  return getShownFields({ schema, modelName, type: 'showDetail', node })
 }
 
-export const getIndexFields = (schema, modelName) => {
-  return getShownFields({ schema, modelName, type: 'showIndex' })
+export const getIndexFields = ({ schema, modelName }) => {
+  const indexFieldOrder = R.prop('indexFieldOrder', getModel(schema, modelName))
+  if (R.type(indexFieldOrder) === 'Function') {
+    return indexFieldOrder({ schema, modelName })
+  }
+  else if (R.type(indexFieldOrder) === 'Array') {
+    return indexFieldOrder
+  }  return getShownFields({ schema, modelName, type: 'showIndex' })
 }
 
 export const getTooltipFields = (schema, modelName) => {
