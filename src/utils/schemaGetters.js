ef39726e8499b84715af72e24cb40d8b582c437a
myurch <36048692+myurch@users.noreply.github.com>
2019-10-28 13:36:18 -0700

    getShownFields

--- src/utils/schemaGetters.js
+++ src/utils/schemaGetters.js
@@ -50,60 +50,60 @@ export const getField = (schema, modelName, fieldName) => (
   )(schema, modelName)
 )
 
-const getShownFields = ({ schema, modelName, type, node = {} }) => {
+const getShownFields = ({ schema, modelName, type, node = {}, data, user, customProps }) => {
   const fieldOrder = R.prop('fieldOrder', getModel(schema, modelName))
   return R.filter(fieldName => {
     let show = R.prop(type, getField(schema, modelName, fieldName))
     if (R.type(show) === 'Function') {
       show = show({
-        schema, modelName, fieldName, node
+        schema, modelName, fieldName, node, data, user, customProps
       })
     }
     return show
   }, fieldOrder)
 }
 
-export const getRequiredFields = (schema, modelName) => {
-  return getShownFields({ schema, modelName, type: 'required' })
+export const getRequiredFields = (schema, modelName, customProps = null) => {
+  return getShownFields({ schema, modelName, type: 'required', customProps })
 }
 
-export const getCreateFields = ({ schema, modelName, user }) => {
+export const getCreateFields = ({ schema, modelName, user, customProps }) => {
   const createFieldOrder = R.prop('createFieldOrder', getModel(schema, modelName))
   if (R.type(createFieldOrder) === 'Function') {
-    return createFieldOrder({ schema, modelName, user })
+    return createFieldOrder({ schema, modelName, user, customProps })
   }
   else if (R.type(createFieldOrder) === 'Array') {
     return createFieldOrder
   }
-  return getShownFields({ schema, modelName, type: 'showCreate' })
+  return getShownFields({ schema, modelName, type: 'showCreate', user, customProps })
 }
 
 export const getHasIndex = (schema, modelName) => {
   return R.prop('hasIndex', getModel(schema, modelName))
 }
 
-export const getDetailFields = ({ schema, modelName, node }) => {
+export const getDetailFields = ({ schema, modelName, node, customProps }) => {
   const detailFieldOrder = R.prop('detailFieldOrder', getModel(schema, modelName))
   if (R.type(detailFieldOrder) === 'Function') {
-    return detailFieldOrder({ schema, modelName, node })
+    return detailFieldOrder({ schema, modelName, node, customProps })
   }
   else if (R.type(detailFieldOrder) === 'Array') {
     return detailFieldOrder
-  }  return getShownFields({ schema, modelName, type: 'showDetail', node })
+  }  return getShownFields({ schema, modelName, type: 'showDetail', node, customProps })
 }
 
-export const getIndexFields = ({ schema, modelName, data, user }) => {
+export const getIndexFields = ({ schema, modelName, data, user, customProps }) => {
   const indexFieldOrder = R.prop('indexFieldOrder', getModel(schema, modelName))
   if (R.type(indexFieldOrder) === 'Function') {
-    return indexFieldOrder({ schema, modelName, data, user })
+    return indexFieldOrder({ schema, modelName, data, user, customProps })
   }
   else if (R.type(indexFieldOrder) === 'Array') {
     return indexFieldOrder
-  }  return getShownFields({ schema, modelName, type: 'showIndex' })
+  }  return getShownFields({ schema, modelName, type: 'showIndex', data, user, customProps })
 }
 
-export const getTooltipFields = (schema, modelName) => {
-  return getShownFields({ schema, modelName, type: 'showTooltip' })
+export const getTooltipFields = (schema, modelName, customProps = null) => {
+  return getShownFields({ schema, modelName, type: 'showTooltip', customProps })
 }
 export const getEnumChoices = (schema, modelName, fieldName) => {
   return R.prop('choices', getField(schema, modelName, fieldName))
