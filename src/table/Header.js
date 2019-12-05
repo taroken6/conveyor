c0480eea560cf47ba1cca128ed61086f9ff56882
Tina Rickard <tinayrickard@gmail.com>
2019-12-05 10:44:39 -0800

    (95) can now sort by clicking on field name

--- src/table/Header.js
+++ src/table/Header.js
@@ -3,9 +3,9 @@ import { getFieldLabel, getActions, getField, getFieldConditions } from '../util
 import { showButtonColumn } from './Table'
 import * as R from 'ramda'
 import { isRel } from '../utils/isType'
-import { FilterComp, isColFilterable } from './Filter'
 import { SortButton, isSortable } from './Sort'
 import { shouldDisplay } from '../Utils'
+import { getNextSortKey } from './Sort.js'
 
 export const THead = ({
   schema,
@@ -17,17 +17,11 @@ export const THead = ({
   data,
   tableOptions,
   sortable,
-  filterable,
-  selectOptions,
   fromIndex,
   customProps
 }) => {
   const actions = getActions(schema, modelName)
   const onSort = R.path(['tableOptions', 'sort'], actions)
-  const onFilterChange = R.path(['tableOptions', 'filterChange'], actions)
-  const onFilterSubmit = R.path(['tableOptions', 'filterSubmit'], actions)
-  const onFilterRadio = R.path(['tableOptions', 'filterRadio'], actions)
-  const onMenuOpen = R.path(['input', 'onMenuOpen'], actions)
   return(
     <thead>
       <tr>
@@ -51,35 +45,23 @@ export const THead = ({
           }
 
           const isRelField = isRel(getField(schema, modelName, fieldName))
-          const filterInput = R.path(['filter', modelName, fieldName], tableOptions)
           const sortKeyObj = R.path(['sort', modelName], tableOptions)
           return (
             <th key={idx} style={{ minWidth: '130px' }}>
               <Header
                 {...{
-                  schema,
                   modelName,
                   fieldName,
                   title: getFieldLabel({
                     schema, modelName, fieldName, data, customProps
                   }), // this is the actual 'data' list, not 'node'
-                  onFilterChange: (evt) => onFilterChange({
-                    modelName,
-                    ...evt
-                  }),
-                  onFilterSubmit,
-                  onFilterRadio,
                   onSort,
-                  onMenuOpen,
                   showSort: (
                     tableOptions &&
                     sortable &&
                     isSortable({schema, modelName, fieldName})
                   ) ? !isRelField : false,
-                  showFilter: isColFilterable({schema, modelName, fieldName, tableOptions, filterable}),
                   sortKeyObj,
-                  filterInput,
-                  selectOptions,
                 }}
               />
             </th>
@@ -90,31 +72,35 @@ export const THead = ({
 )}
 
 export const Header = ({
-  schema,
   modelName,
   fieldName,
   title,
-  onFilterChange,
-  onFilterSubmit,
-  onFilterRadio,
   onSort,
-  onMenuOpen,
   showSort,
-  showFilter,
   sortKeyObj,
-  filterInput,
-  selectOptions
-}) => (
-  <div className='header'>
-    <div className='title' >
-      <a style={{ float: 'left', fontSize: '.9em' }}
-        href={'#'}>
-        {title}
-      </a>
+}) => {
+  let sortKey = undefined
+  if (R.prop('fieldName', sortKeyObj) === fieldName) {
+    sortKey = R.prop('sortKey', sortKeyObj)
+  }
+  return (
+    <div
+      className='header'
+      onClick={() => !showSort ? {} : onSort({
+        modelName,
+        fieldName,
+        sortKey: getNextSortKey(sortKey)
+      })}
+    >
+      <div className='title' >
+        <a style={{ float: 'left', fontSize: '.9em' }}
+          href={'#'}>
+          {title}
+        </a>
+      </div>
       <div className={'header-overflow'}>
         { showSort && <SortButton {...{ modelName, fieldName, onSort, sortKeyObj }} /> }
-        { showFilter && <FilterComp {... { fieldName, modelName, schema, onFilterChange, onFilterSubmit, onFilterRadio, onMenuOpen, filterInput, selectOptions }} /> }
       </div>
     </div>
-  </div>
-)
+  )
+}
