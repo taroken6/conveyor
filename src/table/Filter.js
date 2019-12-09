23c9a6f89471b39dda60f617e50bcecd862cc907
Tina Rickard <tinayrickard@gmail.com>
2019-12-09 15:16:51 -0800

    (93) fixed dropdown bug

--- src/table/Filter.js
+++ src/table/Filter.js
@@ -111,7 +111,7 @@ const formatFilter = ({
     ? { label: null, value: null }
     : toOptions(filterOrder[index])
   return (
-    <li key={index} className='list-group-item filter'>
+    <li key={`${index}-${modelName}-${fieldName}-format-filter`} className='list-group-item filter'>
       <div className='filter-fieldname-dropdown'>
         <div className='w-100'>
           <FlexibleInput {...{
