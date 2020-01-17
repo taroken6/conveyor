4b1322b86c327c444c9687e95da311a5d9373b16
myurch <36048692+myurch@users.noreply.github.com>
2020-01-17 15:07:40 -0800

    pagination

--- src/Pagination.js
+++ src/Pagination.js
@@ -3,18 +3,81 @@ import * as R from 'ramda'
 import 'bootstrap'
 import { getActions } from './utils/schemaGetters'
 
-export const Pagination = ({ schema, modelName, ...props }) => {
-  const actions = getActions(schema, modelName)
+const PaginationLink = ({ modelName, changePage, text, updatedPageIndex }) => {
+  return (
+    <li className="page-item">
+      <a className="page-link" href="#" onClick={() => changePage({
+        modelName,
+        updatedPageIndex
+      })}>
+        {text}
+      </a>
+    </li>
+  )
+}
 
-  console.log('conveyor props', props)
+export const Pagination = ({ schema, modelName, tableView }) => {
+  const actions = getActions(schema, modelName)
+  const changePage = R.path(['tableOptions', 'changePage'], actions)
+
+  // current page idx
+  const idx = R.pathOr(0, ['page', modelName], tableView)
+
+  // get index of last hypothetical data point
+  const totalDataLength = R.prop('totalDataLength', tableView)
+  const paginationAmt = R.prop('paginationAmt', tableView)
+  const lastIndex = Math.floor((totalDataLength - 1) / paginationAmt)
+
+  // get previous & last conditions
+  const hasPrevious = idx > 0
+  const hasLast = (lastIndex > 0) && (idx !== lastIndex)
+
+  console.log('totalDataLength', totalDataLength)
+  console.log('lastIndex', lastIndex)
+  console.log('idx', idx)
+  console.log('hasPrev', hasPrevious)
+  console.log('hasLast', hasLast)
+
+  // no pagination
+  if (!hasPrevious && !hasLast) {
+    return null
+  }
+
+  //'←'
+  //'⇤'
+  //'→'
+  //'⇥'
+  //'«' ... '»'
+  //'‹' ... '›'
   return (
     <nav aria-label="Page navigation example">
       <ul className="pagination">
-        <li className="page-item"><a className="page-link" href="#">Previous</a></li>
-        <li className="page-item"><a className="page-link" href="#">1</a></li>
-        <li className="page-item"><a className="page-link" href="#">2</a></li>
-        <li className="page-item"><a className="page-link" href="#">3</a></li>
-        <li className="page-item"><a className="page-link" href="#">Next</a></li>
+        {
+          hasPrevious && <PaginationLink {...{
+            modelName,
+            changePage,
+            text: '‹',
+            updatedPageIndex: (idx - 1)
+          }} />
+        }
+
+        {
+          <PaginationLink {...{
+            modelName,
+            changePage,
+            text: `${idx}`,
+            updatedPageIndex: (idx)
+          }} />
+        }
+
+        {
+          hasLast && <PaginationLink {...{
+            modelName,
+            changePage,
+            text: '›',
+            updatedPageIndex: (idx + 1)
+          }} />
+        }
       </ul>
     </nav>
   )
