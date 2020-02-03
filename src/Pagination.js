2a09e0b8a47d4ab4eaac64dc311a081801fa95b8
Tina Rickard <tinayrickard@gmail.com>
2020-02-03 13:45:57 -0800

    (148) began adding middle tooltip

--- src/Pagination.js
+++ src/Pagination.js
@@ -1,17 +1,40 @@
 import React from 'react'
 import * as R from 'ramda'
 import { getActions } from './utils/schemaGetters'
+import { Tooltip } from 'react-tippy'
+import FlexibleInput from './input'
+import { inputTypes } from './consts'
+
+const TooltipContent = ({ modelName, fieldName, onChangePage, text, updatedPageIndex }) => {
+  if (isNaN(text)) {
+    return <span>{`Page ${updatedPageIndex + 1}`}</span>
+  }
+  return <span />
+}
 
 const PaginationLink = ({ modelName, fieldName, onChangePage, text, updatedPageIndex }) => {
-  return (
-    <li className="page-item">
-      <a className="page-link" href="#" onClick={() => onChangePage({
-        modelName, fieldName, updatedPageIndex
-      })}>
-        {text}
-      </a>
-    </li>
-  )
+  // const content = <TooltipContent {...{ modelName, fieldName, onChangePage, text, updatedPageIndex }} />
+  const link = <a
+    className='page-link'
+    href='#'
+    onClick={() => onChangePage({
+      modelName, fieldName, updatedPageIndex
+    })}
+  >
+    {text}
+  </a>
+  if (isNaN(text)) {
+    return (
+      <Tooltip
+        html={<span>{`Page ${updatedPageIndex + 1}`}</span>}
+        delay={0}
+        interactive
+      >
+        {link}
+      </Tooltip>
+    )
+  }
+  return link
 }
 
 export const Pagination = ({ modelName, fieldName = null, idx, lastIndex, onChangePage }) => {
