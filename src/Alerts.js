5613f27632ec93ff96f54b6543b6bb9bea55a0e6
trickardy <tinayrickard@gmail.com>
2020-10-14 10:17:15 -0700

    added alert component

---
+++ src/Alerts.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export const Alerts = ({ alerts, dismissAlert }) => (
+  <div id='alerts'>
+    {alerts.map((alert, idx) => (
+      <div
+        key={`alert-${idx}`}
+        className={`alert fade show alert-${alert.type}`} role='alert'
+      >
+        {alert.message}
+        <button type='button' className='close' aria-label='Close' onClick={() => dismissAlert(alert)}>&times;</button>
+      </div>
+    ))}
+  </div>
+)
+
+export default Alerts
